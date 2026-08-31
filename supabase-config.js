e4e3e5da5fa40c447cec2f9a23d43cbd1dd26fa9
suraj199-acdc <mepurman@gmail.com>
2026-08-31 10:56:41 +0545

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,13 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://bzdsgawfbgtfygrpvdfs.supabase.co";
+const SUPABASE_URL =
+    "https://bzdsgawfbgtfygrpvdfs.supabase.co";
 
-const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY =
+    "YOUR_PUBLISHABLE_KEY_HERE";
 
-export const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+export const supabase =
+    createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
